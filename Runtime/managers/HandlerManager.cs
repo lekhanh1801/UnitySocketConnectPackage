ff0ce3db8d3610b27f9daad8a38799751ea85165
PHAN THANH. DAT <datpt@hoanglinhonline.com>
2025-12-03 10:18:52 +0700

    add system handler

--- Runtime/managers/HandlerManager.cs
+++ Runtime/managers/HandlerManager.cs
@@ -10,14 +10,16 @@ namespace WebSocketClientPackage.Runtime.managers
 {
     public class HandlerManager
     {
-        private readonly Dictionary<int, IServerResponeHandler> _requestHandlers;
+        private readonly Dictionary<int, IServerResponeHandler> _responseHandlers;
+        private readonly Dictionary<int, IServerResponeHandler> _systemHandlers;
 
         /// <summary>
         ///     Khởi tạo HandlerManager
         /// </summary>
         private HandlerManager()
         {
-            _requestHandlers = new Dictionary<int, IServerResponeHandler>();
+            _responseHandlers = new Dictionary<int, IServerResponeHandler>();
+            _systemHandlers = new Dictionary<int, IServerResponeHandler>();
         }
 
         /// <summary>
@@ -37,26 +39,36 @@ namespace WebSocketClientPackage.Runtime.managers
         /// <param name="handlerType">Type của handler</param>
         public void AddServerResponseHandler(int cmd, Type handlerType)
         {
-            if (Activator.CreateInstance(handlerType) is IServerResponeHandler handler) _requestHandlers[cmd] = handler;
+            if (Activator.CreateInstance(handlerType) is IServerResponeHandler handler) _responseHandlers[cmd] = handler;
         }
 
         /// <summary>
-        ///     Thêm client request handler (generic version)
+        ///     Thêm server response handler (generic version)
         /// </summary>
         /// <typeparam name="T">Handler type</typeparam>
         /// <param name="cmd">Command string</param>
         public void AddServerResponseHandler<T>(int cmd) where T : IServerResponeHandler, new()
         {
-            _requestHandlers[cmd] = new T();
+            _responseHandlers[cmd] = new T();
+        }
+
+        public void AddSystemResponseHandler<T>(int cmd) where T : IServerResponeHandler, new()
+        {
+            _systemHandlers[cmd] = new T();
         }
 
         /// <summary>
-        ///     Xóa sự kiện lắng nghe request từ client
+        ///     Xóa sự kiện lắng nghe response từ server
         /// </summary>
         /// <param name="cmd">Command string</param>
         public void RemoveServerResponseHandler(int cmd)
         {
-            _requestHandlers.Remove(cmd);
+            _responseHandlers.Remove(cmd);
+        }
+
+        public void RemoveSystemResponseHandler(int cmd)
+        {
+            _systemHandlers.Remove(cmd);
         }
 
         /// <summary>
@@ -71,7 +83,13 @@ namespace WebSocketClientPackage.Runtime.managers
             {
                 int controllerId = kMsg.GetControllerId();
                 int requestId = kMsg.GetRequestId();
-                if (_requestHandlers.TryGetValue(requestId, out IServerResponeHandler handler))
+                if (controllerId == StaticController.SYSTEM_CONTROLLER && _systemHandlers.TryGetValue(requestId, out IServerResponeHandler systemHandler))
+                {
+                    systemHandler.HandleServerRespone(kMsg);
+                    return true;
+                } 
+                
+                if (controllerId == StaticController.EXTENSION_CONTROLLER && _responseHandlers.TryGetValue(requestId, out IServerResponeHandler handler))
                 {
                     handler.HandleServerRespone(kMsg);
                     return true;
